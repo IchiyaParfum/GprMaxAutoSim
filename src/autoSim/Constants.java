5763ba2d479550d0543e6e41536f7aaff9dcdc62
mstieger <mstieger@10.0.0.36>
2018-11-05 17:22:57 +0100

    Changed the location of python interpreter in the exeBScan & exeMerge String to the anaconda environment for gprMax (tested)

--- src/autoSim/Constants.java
+++ src/autoSim/Constants.java
@@ -1,10 +1,11 @@
 package autoSim;
 
 public interface Constants {
-	public final static String workingDir = "/";	//Has to end with a /
+	public final static String workingDir = "sim/";	//Has to end with a /
 	public final static String config = "GprMax.config";
-	public final static String exeBScan = "python C:\\Program Files\\gprMax\\gprMax\\gprMax.py";
-	public final static String exeMerge = "python C:\\Program Files\\gprMax\\tools\\outputfiles_merge.py";
+	public final static String exeBScan = "C:\\Users\\mstieger\\Anaconda3\\envs\\gprMax\\python.exe -m gprMax";
+	public final static String exeMerge = "C:\\Users\\mstieger\\Anaconda3\\envs\\gprMax\\python.exe -m "
+			+ "tools.outputfiles_merge";
 	public final static String exePlot = "matlab -nodisplay -nosplash -nodesktop -noFigureWindow -wait -r \"try, "
 			+ "run('C:\\Program Files\\gprMax\\tools\\MATLAB_scripts\\plot_Bscan.m'), catch me, fprintf('%s / %s\\n',me.identifier,me.message), end, exit\"";
 	
