13413368454a07e9c45fd5b96779f0d3b4d61bfb
mstieger <mstieger@DESKTOP-3G6S1BK.ntb.ch>
2019-01-07 10:13:33 +0100

    Created logger instance for tests (not used)

--- src/autoSim/StreamExhauster.java
+++ src/autoSim/StreamExhauster.java
@@ -1,8 +1,11 @@
 package autoSim;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**Because some native platforms only provide limited buffer size for standard input and output streams,
  * failure to promptly write the input stream or read the output stream of the subprocess may cause 
@@ -17,9 +20,11 @@ class StreamExhauster extends Thread
     InputStream is;
     char[] buffer;
     boolean running;
+    private final  Logger LOGGER;
     
     StreamExhauster(InputStream is, int bufferSize)
     {
+    	LOGGER = Logger.getLogger(StreamExhauster.class.getName());
         this.is = is;
         buffer = new char[bufferSize];
         running = false;
@@ -31,10 +36,11 @@ class StreamExhauster extends Thread
     	running = true;
     	
     	//Reads from stream
-		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader isr = new BufferedReader(new InputStreamReader(is));
     	try {
             while(running){
             	isr.read(buffer);
+            	//LOGGER.log(Level.INFO, buffer.toString()); TODO
             }
     	}catch(IOException e) {
     		
